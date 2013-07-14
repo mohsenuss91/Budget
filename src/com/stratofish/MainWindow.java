92f77664ee6f29adc415b76395083a5b679317e6
Dan Brown <dan@harthinian.com>
2013-07-14 12:03:37 +0100

    Added menu stubs. Conenction to SQLlite added

--- src/com/stratofish/MainWindow.java
+++ src/com/stratofish/MainWindow.java
@@ -5,24 +5,47 @@ import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.KeyEvent;
+import java.sql.Connection;
 
 import javax.swing.*;
 
 public class MainWindow extends JFrame implements ActionListener, ItemListener
 {
-  private static final long serialVersionUID = 1L;
-  private JMenuBar menuBar;
+  protected static final long serialVersionUID = 1L;
+  
+  protected JMenuBar menuBar;
+  protected JMenuItem newMenuItem;
+  protected JMenuItem openMenuItem;
+  protected JMenuItem saveMenuItem;
+  protected JMenuItem exitMenuItem;
+
+  protected BudgetFile budgetFile = null;
+  protected Connection conn = null;
 
   MainWindow()
 	{
-  	JLabel jlbHelloWorld = new JLabel("Hello World");
-  	add(jlbHelloWorld);
-  	setSize(1024, 768);
-  	
-  	AddMenu();
-  	
-  	setVisible(true);
+  	CreateWindowComponents();
 	}
+  
+  private void CreateWindowComponents()
+  {
+    AddMenu();
+    
+    JSplitPane sp = new JSplitPane();
+    
+    add(sp);
+    
+    JPanel panel1 = new JPanel();
+    sp.setLeftComponent(panel1);
+    JPanel panel2 = new JPanel();
+    sp.setRightComponent(panel2);
+    
+    JLabel jlbHelloWorld = new JLabel("Hello World");
+    panel1.add(jlbHelloWorld);
+    setSize(1024, 768);
+    
+    setVisible(true);
+  }
 
   private void AddMenu()
   {
@@ -35,24 +58,24 @@ public class MainWindow extends JFrame implements ActionListener, ItemListener
     menuBar.add(fileMenu);
 
     // Items
-    JMenuItem newMenuItem = new JMenuItem("New", KeyEvent.VK_N);
+    newMenuItem = new JMenuItem("New", KeyEvent.VK_N);
     newMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
     newMenuItem.addActionListener(this);
     fileMenu.add(newMenuItem);
     
-    JMenuItem openMenuItem = new JMenuItem("Open", KeyEvent.VK_O);
+    openMenuItem = new JMenuItem("Open", KeyEvent.VK_O);
     openMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK));
     openMenuItem.addActionListener(this);
     fileMenu.add(openMenuItem);
     
-    JMenuItem saveMenuItem = new JMenuItem("Save", KeyEvent.VK_S);
+    saveMenuItem = new JMenuItem("Save", KeyEvent.VK_S);
     saveMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
     saveMenuItem.addActionListener(this);
     fileMenu.add(saveMenuItem);
     
     fileMenu.addSeparator();
     
-    JMenuItem exitMenuItem = new JMenuItem("Exit", KeyEvent.VK_X);
+    exitMenuItem = new JMenuItem("Exit", KeyEvent.VK_X);
     exitMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F4, ActionEvent.ALT_MASK));
     exitMenuItem.addActionListener(this);
     fileMenu.add(exitMenuItem);
@@ -65,6 +88,39 @@ public class MainWindow extends JFrame implements ActionListener, ItemListener
     
   }
 
+  protected void New()
+  {
+    System.out.println("New");
+    
+    try
+    {
+      budgetFile = new BudgetFile();
+    } catch (ClassNotFoundException e)
+    {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    
+    budgetFile.New("newbudget");
+    
+    conn = BudgetFile.Instance();
+  }
+  
+  protected void Open()
+  {
+    System.out.println("Open");
+  }
+  
+  protected void Save()
+  {
+    System.out.println("Save");
+  }
+  
+  protected void Exit()
+  {
+    System.out.println("Exit");
+  }
+  
   @Override
   public void itemStateChanged(ItemEvent arg0)
   {
@@ -74,6 +130,23 @@ public class MainWindow extends JFrame implements ActionListener, ItemListener
   @Override
   public void actionPerformed(ActionEvent arg0)
   {
-    System.out.println("New clicked");
+    JMenuItem source = (JMenuItem) arg0.getSource();
+    
+    if (source == newMenuItem)
+    {
+      New();
+    }
+    if (source == openMenuItem)
+    {
+      Open();
+    }
+    if (source == saveMenuItem)
+    {
+      Save();
+    }
+    if (source == exitMenuItem)
+    {
+      Exit();
+    }
   }
 }
