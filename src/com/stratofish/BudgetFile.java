0363cd95816c79eba1bc23b468e48d1afacbc22e
Richard Talbutt <richard@talbutt.co.uk>
2013-07-14 15:23:09 +0100

    Added new method "close" to budgetFile.

--- src/com/stratofish/BudgetFile.java
+++ src/com/stratofish/BudgetFile.java
@@ -21,42 +21,46 @@ public class BudgetFile
     return connection;
   }
   
+  public static void Close()
+  {
+    try
+    {
+      if(connection != null)
+         connection.close();
+      }
+    catch(SQLException e)
+    {
+      // connection close failed.
+      System.err.println(e);
+    }
+    
+    connection = null;
+  }
+  
   public void New(String name) 
   {     
     try
     {
-      if (connection == null)
-      {      
-        connection = DriverManager.getConnection("jdbc:sqlite:" + name + ".db");
-      }
+      Close();
+      connection = DriverManager.getConnection("jdbc:sqlite:" + name + ".db");     
       
       Statement statement = connection.createStatement();
-      statement.setQueryTimeout(30);  // set timeout to 30 sec.
+      
+      System.out.println("Statement => " + statement);
+            
+      statement.setQueryTimeout(30);  // Set timeout to 30 sec.           
   
       statement.executeUpdate("drop table if exists singlePayments");
       statement.executeUpdate("create table singlePayments(id integer, amount real, description text, paymentTypeId integer, verified integer, dateStamp integer)");
 
       statement.executeUpdate("drop table if exists paymentTypes");
-      statement.executeUpdate("create table paymentTypes(id integer, name text)");    
+      statement.executeUpdate("create table paymentTypes(id integer, name text)");
     }
     catch(SQLException e)
     {
-      // if the error message is "out of memory", 
+      // If the error message is "out of memory", 
       // it probably means no database file is found
       System.err.println(e.getMessage());
-    }
-    finally
-    {
-      try
-      {
-        if(connection != null)
-          connection.close();
-      }
-      catch(SQLException e)
-      {
-        // connection close failed.
-        System.err.println(e);
-      }
     }
   }
 }
